5a530f08348ea9bf32804734c9fba20deb523259
Thanabordi Konlak <68103646+AnoAon@users.noreply.github.com>
2020-07-13 22:32:00 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>;
+
+using namespace std;
+
+void main() {
+	cout << "     #    " << endl;
+	cout << "   #   #  " << endl;
+	cout << "  #     # " << endl;
+	cout << " #       #" << endl;
+	cout << " #########" << endl;
+	cout << " #       #" << endl;
+	cout << "Tanabordee Konlak \n";
+	system("pause");
+}
